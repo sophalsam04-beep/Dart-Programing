ae96d56af92d7eaf24a4d3e5d11e9cb817096452
Phal Developer <sophalsam04@gmail.com>
2026-06-07 23:12:31 +0700

    Create 34. SliverAppBar.dart

---
+++ 34. SliverAppBar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Flutter Apps"),
+              background: FlutterLogo(),
+            ),
+          ),
+
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return ListTile(title: Text("Item : ${index}"));
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
