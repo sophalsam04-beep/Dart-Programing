02520ef651c8b0a6ceefcf4ee77cc11798ffef57
Phal Developer <sophalsam04@gmail.com>
2026-06-08 22:58:28 +0700

    Create 37. Counter_bloc.dart

---
+++ 37. Counter_bloc.dart
@@ -0,0 +1,69 @@
+import 'counter_bloc.dart';
+import 'counter_event.dart';
+import 'counter_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
+
+void main(List<String> args) {
+  runApp(BlocProvider(create: (context) => CounterBloc(), child: App()));
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<CounterBloc>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Bloc in flutter",
+          style: TextStyle(
+            fontSize: 30,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      body: Center(
+        child: BlocBuilder<CounterBloc, CounterState>(
+          builder: (context, state) {
+            return Text("${state.counter}");
+          },
+        ),
+      ),
+
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () => bloc.add(IncrementCounter()),
+            child: Text("+"),
+          ),
+
+          FloatingActionButton(
+            onPressed: () => bloc.add(DecrementCounter()),
+            child: Text("-"),
+          ),
+
+          FloatingActionButton(
+            onPressed: () => bloc.add(ResetCounter()),
+            child: Text("@"),
+          ),
+        ],
+      ),
+    );
+  }
+}
